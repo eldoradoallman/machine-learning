473a067e77fe876d02eb64e8b1ff9cda804b26c1
steffenlaurens <steffenlaurens@gmail.com>
2020-11-29 22:03:15 +0700

    feat: knn algorythm

--- plinko/score.js
+++ plinko/score.js
@@ -1,26 +1,34 @@
 const outputs = [];
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const testSetSize = 10;
-  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  const testSetSize = 100;
+  const k = 10;
 
-  const accuracy = _.chain(testSet)
-    .filter((testPoint) => knn(trainingSet, testPoint[0]) === testPoint[3])
-    .size()
-    .divide(testSetSize)
-    .value();
+  _.range(0, 3).forEach((feature) => {
+    const data = _.map(outputs, (row) => [row[feature], _.last(row)]);
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
+    const accuracy = _.chain(testSet)
+      .filter(
+        (testPoint) =>
+          knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint)
+      )
+      .size()
+      .divide(testSetSize)
+      .value();
 
-  console.log("Accuracy is:", accuracy);
+    console.log("For feature of", feature, "Accuracy is", accuracy);
+  });
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
   return _.chain(data)
-    .map((row) => [distance(row[0], point), row[3]])
+    .map((row) => {
+      return [distance(_.initial(row), point), _.last(row)];
+    })
     .sortBy((row) => row[0])
     .slice(0, k)
     .countBy((row) => row[1])
@@ -33,7 +41,13 @@ function knn(data, point) {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
 }
 
 function splitDataset(data, testCount) {
@@ -43,4 +57,21 @@ function splitDataset(data, testCount) {
   const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount.length; i++) {
+    const column = clonedData.map((row) => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 }
